fd853e4419a0281ada03f3e942d48b64f6e50857
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-09 16:15:56 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #
 apt update -y
-apt install -y libx11-dev libxft-dev  zip git gcc make --no-install-recommends
+apt install -y libx11-dev libxft-dev  zip git gcc make ca-certificates --no-install-recommends
 git clone --depth=1  https://git.suckless.org/tabbed
 cd tabbed
 make
